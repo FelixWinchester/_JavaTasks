7c8009c77a4918515d24eaec36dc9506748151e2
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2025-09-17 01:12:25 +0400

    Update Kadane's Algorithm.java

--- Kadane's Algorithm.java
+++ Kadane's Algorithm.java
@@ -4,7 +4,7 @@ import java.lang.*;
 public class Main {
 
     public static int maxSubArraySum(int[] arr) {
-        if (arr.length == 0) return 0; // защита от пустого массива
+        if (arr.length == 0) return 0; 
 
         int maxSum = arr[0];
         int currentSum = arr[0];
@@ -19,6 +19,6 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {1, -2, 3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubArraySum(arr)); // ожидаем 9
+        System.out.println(maxSubArraySum(arr)); 
     }
 }
