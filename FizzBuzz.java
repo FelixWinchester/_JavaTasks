1c9d0e52f1afdd78db9d0ecc29e22d68810f6042
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2025-09-17 00:23:13 +0400

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i = 1; i < n; i++){
+            if (i % 3 == 0 && n % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+        }
+
+    }
+}
